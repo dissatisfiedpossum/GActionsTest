6315cd742986fdabf51bcef01b219521e92a79a1
dissatisfiedpossum <100021240+dissatisfiedpossum@users.noreply.github.com>
2022-02-20 18:51:31 +0300

    Add files via upload

---
+++ code_for_testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum temp = new Sum();
+
+            Console.WriteLine(temp.GiveResult());
+        }
+    }
+}
---
+++ code_for_testing/Sum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsoleApp
+{
+    public class Sum
+    {
+        int a, b;
+        public Sum()
+        {
+            a = 41;
+            b = 12;
+        }
+
+        public int GiveResult()
+        {
+            int res = 0;
+            res = this.a + this.b;
+            return res;
+        }
+    }
+}
