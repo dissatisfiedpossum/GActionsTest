4100c67db90a480331e9070da02cc5a85b45988c
kekus <bobbiethekot@gmail.com>
2022-02-20 18:32:09 +0300

    commit 1

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            TestConsoleApp.Sum temp = new TestConsoleApp.Sum();
+            int expected = 50;
+            Assert.Equal(expected, temp.GiveResult());
+        }
+    }
+}
