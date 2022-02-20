44137f0700ffc924dae8f97e41c250987b154d32
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-02-20 18:39:34 +0300

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,7 +9,7 @@ namespace XUnitTestProject1
         public void Test1()
         {
             TestConsoleApp.Sum temp = new TestConsoleApp.Sum();
-            int expected = 50;
+            int expected = 53;
             Assert.Equal(expected, temp.GiveResult());
         }
     }
